99728aa90a93ad1ca4780f40d13569b659936873
Guillermo <gperez@libasoles.com>
2017-03-30 16:10:34 -0300

    New use case: swf videos

---
+++ changelog.md
@@ -0,0 +1,8 @@
+
+#changelog
+
+#### 2017-3-30  - 1.1 Released
+
+    - Added a second case, for swf videos. 
+
+#### March 2017 - 1.0 Released 
--- content_script.js
+++ content_script.js
@@ -1,22 +1,47 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    
+    /**
+     * Let's try to find to find the video in some different scenarios.
+     */
     if (request.action == "fullscreenVideo") {
 
-        // access first iframe 
+        var elem; // object containing the url
+        var src; // the actual url of the resource
+        
+        // access main iframe 
         var iframe = document.getElementById('contentBody');
         var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-        // access second iframe
-        iframe = innerDoc.getElementById('InnerUrlId');
-        innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+        // try to detect video in different scenarios
+        elem = innerDoc.getElementById('myElementVideo');
+        if(elem != null) {
+            
+            /** override main frame content with flash content
+             * (can't trigger swf fullscreen mode from js) 
+             */
+            iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
+            iframe.document.open();
+            iframe.document.write(elem.outerHTML);
+            iframe.document.close();
+            
+        } else {
+            
+            // access second iframe
+            iframe = innerDoc.getElementById('InnerUrlId');
+            innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-        // get video iframe
-        iframe = innerDoc.getElementById('video720').getElementsByTagName('iframe')[0];
-
-        // open video in new window, fullscreen
-        window.open(iframe.src);
-
+            // get video iframe
+            elem = innerDoc.getElementsByTagName('iframe')[0];
+            
+            // get video source
+            src = elem.src;
+            
+            // open video in new window, fullscreen
+            window.open(src);
+        }        
+        
         sendResponse({
-            msg: "Opened video in new window"
+            msg: "Done"
         });
     } else {
         sendResponse({}); // Send nothing..
