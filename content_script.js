d66464487a926742213c144af611146494af5c05
Guillermo <gperez@libasoles.com>
2017-03-31 21:23:39 -0300

    Improved style. v1.4

--- content_script.js
+++ content_script.js
@@ -2,10 +2,10 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     
     let response = { msg: null }; // defaults to nothing
         
-    console.log(request.action, request);
+    console.log("Request: ", request.action, request);
         
     /**
-     * Let's try to find to find the video in some different scenarios.
+     * Call to proper method
      */
     switch (request.action) 
     {
@@ -21,9 +21,24 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
             response.msg = forum.highlightTitles(request);
             break;
             
+        case "improveArrows":
+            response.msg = forum.improveArrows(request);
+            break;  
+            
+        case "improveForum":
+            forum.changeTextSize(request);
+            forum.highlightTitles(request);
+            forum.improveArrows(request);
+            forum.improveButtons(request);
+            forum.highlightFrame(request);
+            response.msg = 1;
+            break;     
+            
         default:
             break;    
     }
+    
+    console.log("Response: ", response);
     
     sendResponse(response); 
 });
@@ -80,14 +95,21 @@ let video = {
  */
 let forum = {
     
-    getPageIframe: function() {
+    getMainIframe: function() {
         
         let iframe = document.getElementById("contentBody");   
         
         iframe = iframe.contentDocument || iframe.contentWindow.document;
         
-        iframe = iframe.getElementById("rawContent");
+        return iframe;
+    },
+    
+    getPageIframe: function() {
         
+        let iframe = this.getMainIframe();
+        
+        // get secondary iframe
+        iframe = iframe.getElementById("rawContent");        
         iframe = iframe.contentDocument || iframe.contentWindow.document;
         
         let page = iframe.getElementById("forum_page");
@@ -105,18 +127,19 @@ let forum = {
     
         let iframe = this.getPageIframe();
         
-        let page = iframe.getElementById("forum_page");
-        
-        if(page != null) {
+        if(iframe != null) {
                     
             if(iframe.body.classList.contains("text-resized"))
                 return true;
             
             let sheet = iframe.createElement('style');
-            sheet.innerHTML = "body.text-resized #message-tree span,\n" 
-                            + "body.text-resized #message-tree div {\n"
-                            + "    font-size: 17px !important;\n"
-                            + "}";
+            
+            sheet.innerHTML = `
+                body.text-resized #message-tree span,
+                body.text-resized #message-tree div {
+                    font-size: 17px !important;
+                }
+            `;
             
             iframe.head.appendChild(sheet); // append in head
 
@@ -130,21 +153,144 @@ let forum = {
     
         let iframe = this.getPageIframe();
         
-        let page = iframe.getElementById("forum_page");
-        
-        if(page != null) {                   
+        if(iframe != null) {                   
+            
+            if(iframe.body.classList.contains("titles-highlighted"))
+                return true;
             
             let sheet = iframe.createElement('style');
     
-            sheet.innerHTML = "#message-tree .msg_summary_line {\n"
-                            + "     background-color: #bcf;\n"
-                            + "}";
+            sheet.innerHTML = `
+                #message-tree .msg_summary_line {
+                    background-color: #bcf;
+                    color: #565050;
+                }
+            `;
+
+            iframe.head.appendChild(sheet); // append in head  
+            
+            iframe.body.classList.add("titles-highlighted");     
+        }    
+        
+        return true; 
+    },    
+    
+    improveArrows: function (request) {
+    
+        let iframe = this.getPageIframe();
+       
+        if(iframe != null) {                   
+            
+            if(iframe.body.classList.contains("arrows-improved"))
+                return true;
+            
+            let sheet = iframe.createElement('style');
+    
+            sheet.innerHTML = `
+                #forum_page .x-tree-no-lines .x-tree-ec-icon {
+                    margin: 0 20px;
+                    border: solid;
+                    background: #fff;
+                    border-width: 0px 3px 3px 0 !important;   
+                    width: 10px;
+                    height: 10px;                    
+                    padding-top: 0 !important;
+                    border-color: #2d67b0;
+                }
+
+                    #forum_page .x-tree-no-lines .x-tree-elbow,
+                    #forum_page .x-tree-no-lines .x-tree-elbow-end {
+                        border: none !important;
+                    }
+                
+                    #forum_page .x-tree-no-lines .x-tree-elbow-plus {       
+                        transform: rotate(45deg);
+                        -webkit-transform: rotate(45deg);
+                        margin-top: -4px !important;
+                    }
+
+                    #forum_page .x-tree-no-lines .x-tree-elbow-minus {       
+                        transform: rotate(-135deg);
+                        -webkit-transform: rotate(-135deg);
+                        margin-top: 9px !important;
+                    }
+            `;
+
+            iframe.head.appendChild(sheet); // append in head  
+            
+            iframe.body.classList.add("arrows-improved");
+        }    
+        
+        return true; 
+    },
+    
+    improveButtons: function () {
+        
+        let iframe = this.getPageIframe();
+       
+        if(iframe != null) {                   
+            
+            if(iframe.body.classList.contains("buttons-improved"))
+                return true;
+            
+            let sheet = iframe.createElement('style');
+    
+            sheet.innerHTML = `
+                #message-tree .msg_action {      
+                    padding-top: 5px;
+                    padding-bottom: 5px;
+                }
+                
+                #message-tree .x-tree-node a:hover {      
+                    color: #454545 !important;
+                }
+                
+                #forum_page .x-tree-no-lines .x-tree-elbow-plus {  
+                    margin-top: -13px !important;
+                }
+
+                #forum_page .x-tree-no-lines .x-tree-elbow-minus {   
+                    margin-top: 2px !important;
+                }
+            `;
 
             iframe.head.appendChild(sheet); // append in head    
+            
+            iframe.body.classList.add("buttons-improved");
+        }    
+        
+        return true;             
+    },
+    
+    highlightFrame: function (request) {
+    
+        let iframe = this.getMainIframe();
+       
+        if(iframe != null) {                   
+            
+            if(iframe.body.classList.contains("frame-highlighted"))
+                return true;
+            
+            let sheet = iframe.createElement('style');
+    
+            sheet.innerHTML = `
+                body > div {      
+                    background: #ccc; /* For browsers that do not support gradients */
+                    background: -webkit-linear-gradient(#ccc, #aaa); /* For Safari 5.1 to 6.0 */
+                    background: -o-linear-gradient(#ccc, #aaa); /* For Opera 11.1 to 12.0 */
+                    background: -moz-linear-gradient(#ccc, #aaa); /* For Firefox 3.6 to 15 */
+                    background: linear-gradient(#ccc, #aaa); /* Standard syntax */
+                }
+            `;
+
+            iframe.head.appendChild(sheet); // append in head  
+            
+            iframe.body.classList.add("frame-highlighted");
         }    
         
         return true; 
     }
 }
+
 
 
